4dcbd1d5ffda98a27ee65be4e3cde26b30b17525
Admin <subashbaidya9@gmail.com>
2023-05-03 13:09:55 +0545

    Mongoose, Env, Envalid Setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
-import express from 'express';
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./utils/validateEnv";
+import mongoose from "mongoose";
 
-app.get('/', (req,res)=>{
-    res.send("Hello, World");
-});
+const port = env.PORT;
 
-app.listen(port,()=>{
-    console.log(`Server running on port: ${port}`)
-})
+//Connection with MongoDB
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("MongoDB Connection Successful");
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`);
+        });
+    })
+    .catch(console.error);
+
