bdf9fe1757f6523a50c3ec6f126f5b3fc9917447
Admin <subashbaidya9@gmail.com>
2023-05-05 10:06:48 +0545

    Http Errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import taskRoutes from "./routes/task";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
@@ -10,15 +11,19 @@ app.use(express.json())
 app.use('/api/tasks', taskRoutes)
 
 app.use((req,res,next)=>{  
-    next(Error("Endpoint doesnot exists"))
+    next(createHttpError(404,"Endpoint not found."));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction)=>{
     console.error(error);
     let errorMessage = 'An internal error has occured';
-    if(error instanceof Error) errorMessage = error.message;
-    res.status(500).json({error: errorMessage})
+    let statusCode = 500;
+    if(isHttpError(error)){
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({error: errorMessage})
 });
 
 export default app;
--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import TaskModel from "../models/task";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getTasks: RequestHandler = async (req,res,next)=>{
     try{
@@ -13,16 +15,30 @@ export const getTasks: RequestHandler = async (req,res,next)=>{
 export const getTaskById: RequestHandler = async(req, res, next) =>{
     const { taskId } = req.params;
     try {
+        if(!mongoose.isValidObjectId(taskId)){
+            throw createHttpError(400,"Invalid Task Id");
+        }
         const task = await TaskModel.findById(taskId).exec();
+        if(!task){
+            throw createHttpError(404,"Task not found.");
+        }
         res.status(200).json(task);
     } catch (error) {
         next(error);
     }
 }
 
-export const createTask: RequestHandler = async(req, res, next) =>{
+interface TaskBody{
+    title?: string,
+    description?: string,
+}
+
+export const createTask: RequestHandler<unknown, unknown, TaskBody, unknown> = async(req, res, next) =>{
     const {title, description} = req.body;
     try{ 
+        if(!title){
+            throw createHttpError(400,"Task must have title.");
+        }
         const task = await TaskModel.create({title, description});
         res.status(201).json(task);
     }catch(error){
