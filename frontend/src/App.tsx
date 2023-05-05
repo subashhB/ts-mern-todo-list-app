88f6eabe91b8e44d44be3843346b8eabeccc60c1
Admin <subashbaidya9@gmail.com>
2023-05-05 17:27:51 +0545

    Fetching the data from backend and proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Task } from './models/task';
 
 function App() {
+  const[tasks, setTasks] = useState<Task[]>([]);
+  useEffect(()=>{
+    async function loadTasks() {
+      try{
+        const res = await fetch('/api/tasks');
+        const tasks = await res.json();
+        setTasks(tasks);
+      }catch(error){
+        console.log(error);
+        alert(error);
+      }
+    }
+    loadTasks();
+  },[]);
   return (
     <div className="App">
-      
+      {JSON.stringify(tasks) }
     </div>
   );
 }
---
+++ frontend/src/models/task.ts
@@ -0,0 +1,8 @@
+export interface Task {
+    _id: string,
+    title: string,
+    desciption?: string,
+    completed: boolean,
+    createdAt: string,
+    updatedAt: string
+}
