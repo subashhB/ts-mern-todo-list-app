2a9762e06bb4665f258062b988ae85c450e4e138
Admin <subashbaidya9@gmail.com>
2023-05-06 15:58:13 +0545

    Task Cards Styling

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Task as TaskModel } from '../models/task'
+import { formatDate } from '../utils/formatDate';
 
 interface TaskProps {
     task: TaskModel
 }
 
 const Task = ({ task }: TaskProps) => {
+    let createdUpdatedText: string;
+    if(task.updatedAt > task.createdAt){
+        createdUpdatedText = `Updated: ${formatDate(task.updatedAt)}`
+    }else{
+        createdUpdatedText = `Created: ${formatDate(task.createdAt)}`
+    }
   return (
     <div className={task.completed ? 'todo-list-completed': 'todo-list-incomplete' }>
-        <h4>{ task.title }</h4>
+        <h4>{ task.title }</h4> <span className='status'>{task.completed ? 'Completed': 'ToDo'}</span>
         <p>{ task.description }</p>
+        <hr/>
+        <p style={{fontSize: '0.8em', opacity: '0.7'}}>{ createdUpdatedText }</p>
     </div>
   )
 }
