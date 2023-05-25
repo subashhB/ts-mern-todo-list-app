c923ec369c3499e6491c5003595bd921cafeec93
Admin <subashbaidya9@gmail.com>
2023-05-25 17:01:52 +0545

    feat - Get Authenticated User (Backend)

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,6 +3,19 @@ import UserModel from "../models/user";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 
+export const getAuthenticatedUser: RequestHandler = async(req, res, next) =>{
+    const authenticatedUserId = req.session.userId;
+    try{
+        if(!authenticatedUserId){
+            throw createHttpError(401, "User not Authenticated.");
+        }
+        const user = await UserModel.findById(authenticatedUserId).select("+email").exec();
+        res.status(200).json(user);
+    }catch(error){
+        next(error);
+    }
+}
+
 interface SignUpBody{
     username?: string,
     email?: string,
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,6 +3,7 @@ import * as UserController from "../controllers/userController";
 
 const router = express.Router();
 
+router.get("/", UserController.getAuthenticatedUser);
 router.post("/signup", UserController.signUp);
 router.post("/login", UserController.login);
 router.post("/logout", UserController.logout);
